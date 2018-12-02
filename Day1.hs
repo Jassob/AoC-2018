36514ef53972fa59045d2f41b8828859803987e3
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-12-02 13:44:56 +0100

    Add solution to part 2 of day 1

--- Day1.hs
+++ Day1.hs
@@ -1,6 +1,17 @@
 -- | Solves first day in Advent of Code 2018
 module Main where
 
+import           Control.Monad                  ( foldM )
+import           Control.Monad.State            ( StateT
+                                                , gets
+                                                , modify
+                                                , evalStateT )
+import           Control.Monad.Trans.Except     ( ExceptT
+                                                , runExceptT
+                                                , throwE )
+import           Data.Bool                      ( bool )
+import           Data.HashSet                   ( HashSet )
+import qualified Data.HashSet              as S
 import           Text.Read                      ( readEither )
 
 import Lib
@@ -13,7 +24,7 @@ main = do
 main' :: StandardArgs -> IO Int
 main' (StdArgs fp p)
   | p == 1 = part1 <$> parsedNums
-  | otherwise = undefined
+  | otherwise = part2 =<< parsedNums
   where parsedNums = map readWithSign . lines <$> readFile fp
 
 readWithSign :: String -> Int
@@ -28,7 +39,19 @@ readWithSign (s:num)
           id
           (readEither str)
 
+-- PART 1
 -- | Sums all elements in the list. Caution: may overflow
 part1 :: [Int] -> Int
 part1 = sum
 
+-- PART 2
+part2 :: [Int] -> IO Int
+part2 is = do
+  res <- evalStateT (runExceptT $ foldM go 0 (concat $ repeat is)) S.empty
+  either pure (fail s) res
+  where
+    s = "Could not find a repeating frequency"
+
+    go :: Int -> Int -> ExceptT Int (StateT (HashSet Int) IO) Int
+    go acc num = let cur = acc + num in
+      gets (S.member cur) >>= bool (modify (S.insert cur) >> pure cur) (throwE cur)
