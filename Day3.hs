168b3ed3bc43f8c4a8c6720c9a0508f58711401f
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-12-03 19:48:56 +0100

    Add solution for part 1 of day 3

---
+++ Day3.hs
@@ -0,0 +1,51 @@
+-- | Solves the problems of day 3 of Advent of Code 2018
+module Main where
+
+import           Data.HashMap.Strict            ( HashMap )
+import qualified Data.HashMap.Strict       as M
+
+import Lib
+
+main :: IO ()
+main = do
+   args <- parseArgs
+   putStrLn =<< main' args
+
+main' :: StandardArgs -> IO String
+main' (StdArgs fp p)
+   | p == 1 = show . part1 <$> claims
+   | otherwise = undefined
+   where claims = map read . lines <$> readFile fp
+
+data Claim = Claim
+  { cId :: Int
+  , coords :: [(Int, Int)]
+  }
+
+-- Reads a claim on format #cId @ mLeft,mTop: wxh
+instance Read Claim where
+  readsPrec p ('#':str) =
+    let [(num, r1)] = readsPrec p str
+        [(mLeft, r2)] = readsPrec p $ dropChar r1
+        [(mTop, r3)] = readsPrec p $ dropChar r2
+        [(w, 'x':r4)] = readsPrec p $ dropChar r3
+        [(h, r5)] = readsPrec p r4
+        size = [ (x, y) | x <- [mLeft..mLeft + w - 1], y <- [mTop..mTop + h - 1]]
+    in [(Claim num size, r5)]
+       where dropChar :: String -> String
+             dropChar s = let [(_, rest)] = lex s in rest
+  readsPrec _ _ = []
+
+-- Shows a claim in the same format as read parses
+instance Show Claim where
+  show (Claim i cs) =
+    let mLeft = fst $ head cs
+        mTop = snd $ head cs
+        h = length $ filter ((==mLeft) . fst) cs
+        w = length $ filter ((==mTop) . snd) cs
+    in "#" ++ show i ++ " @ " ++ show mLeft ++ "," ++ show mTop ++ ": " ++ show w ++ "x" ++ show h
+
+part1 :: [Claim] -> Int
+part1 = length . filter (>1) . M.elems . foldr go M.empty
+  where go :: Claim ->  HashMap (Int, Int) Int -> HashMap (Int, Int) Int
+        go c m = foldr (\key -> M.alter (pure . maybe 1 (+1)) key) m (coords c)
