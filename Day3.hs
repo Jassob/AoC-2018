d125eefa2e6b38abc3b1525bc1297d69a141fb02
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-12-04 08:45:02 +0100

    Add solution for part 2 of day 3
    Today's weather: cloudy with a chance of folds

--- Day3.hs
+++ Day3.hs
@@ -3,6 +3,8 @@ module Main where
 
 import           Data.HashMap.Strict            ( HashMap )
 import qualified Data.HashMap.Strict       as M
+import           Data.HashSet                   ( HashSet )
+import qualified Data.HashSet              as S
 
 import Lib
 
@@ -14,12 +16,15 @@ main = do
 main' :: StandardArgs -> IO String
 main' (StdArgs fp p)
    | p == 1 = show . part1 <$> claims
-   | otherwise = undefined
+   | otherwise = show . part2 <$> claims
    where claims = map read . lines <$> readFile fp
 
+type ClaimId = Int
+type Coord = (Int, Int)
+
 data Claim = Claim
-  { cId :: Int
-  , coords :: [(Int, Int)]
+  { cId :: ClaimId
+  , coords :: [Coord]
   }
 
 -- Reads a claim on format #cId @ mLeft,mTop: wxh
@@ -47,5 +52,17 @@ instance Show Claim where
 
 part1 :: [Claim] -> Int
 part1 = length . filter (>1) . M.elems . foldr go M.empty
-  where go :: Claim ->  HashMap (Int, Int) Int -> HashMap (Int, Int) Int
-        go c m = foldr (\key -> M.alter (pure . maybe 1 (+1)) key) m (coords c)
+  where go :: Claim -> HashMap Coord Int -> HashMap Coord Int
+        go c m = foldr (M.alter (pure . maybe 1 (+1))) m (coords c)
+
+part2 :: [Claim] -> [Int]
+part2 cs = S.toList $ M.foldl' (foldr (S.delete)) s overlaps
+  where
+    overlaps = M.filter ((>1) . length) m
+    (m, s) = foldr go (M.empty, S.empty) cs
+
+    go :: Claim -> (HashMap Coord [ClaimId], HashSet ClaimId) -> (HashMap Coord [ClaimId], HashSet ClaimId)
+    go c (m', s')= foldr (go' $ cId c) (m', s') (coords c)
+
+    go' :: ClaimId -> Coord -> (HashMap Coord [ClaimId], HashSet ClaimId) -> (HashMap Coord [ClaimId], HashSet ClaimId)
+    go' cId' crd (m', s') = (M.alter (pure . maybe [cId'] (cId':)) crd m', S.insert cId' s')
